fabf73719d7ce31e34f67cd064f7fc0b544d6d29
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-02-19 20:58:23 -0300

    feat: ensure route /repositories/id not be able to update a non existing repository

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
 const { v4: uuid } = require("uuid");
-
 const app = express();
-
 app.use(express.json());
 
 const repositories = [];
@@ -28,15 +25,15 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = { ...repositories[repositoryIndex], title, url, techs };
 
   repositories[repositoryIndex] = repository;
 
@@ -48,7 +45,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
